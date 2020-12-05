75050856bd442cd96784f87ac37b6e8ddf9e14f4
choisungwook <kgg1959@navaer.com>
2020-12-05 16:00:30 +0900

    Add get 파라미터 파싱

---
+++ 02_get_parameter/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def test():
+    message = request.args.get('msg')
+    return_data = {}
+
+    if message is None:
+      return_data = {'msg': 'empty'}
+    else:
+      return_data = {'msg': message}
+
+    return jsonify(return_data)
+
+if __name__=="__main__":
+  app.run(host="127.0.0.1", port=8989, debug=True)
